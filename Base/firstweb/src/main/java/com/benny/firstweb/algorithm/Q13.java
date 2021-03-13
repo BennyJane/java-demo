08c8364375302df3f83d946e0875996b4786c8dc
BennyJane <3355817143@qq.com>
2021-03-13 22:30:55 +0800

    更新

---
+++ Base/firstweb/src/main/java/com/benny/firstweb/algorithm/Q13.java
@@ -0,0 +1,11 @@
+package com.benny.firstweb.algorithm;
+
+/**
+ * 1003. 检查替换后的词是否有效
+ * https://leetcode-cn.com/problems/check-if-word-is-valid-after-substitutions/
+ *
+ * 题解：
+ * 
+ */
+public class Q13 {
+}
