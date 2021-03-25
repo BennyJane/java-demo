8f969f5800241f38866d9c605ab7adeeccfcbc3f
BennyJane <3355817143@qq.com>
2021-03-25 23:33:04 +0800

    更新

---
+++ Base/src/main/java/com/benny/learning/stringDemo/ComDemo.java
@@ -0,0 +1,24 @@
+package com.benny.learning.stringDemo;
+
+import java.util.Arrays;
+
+public class ComDemo {
+    public static void main(String[] args) {
+        splitTest();
+    }
+
+    static void splitTest() {
+        String str = "benny？jane";
+        String[] res;
+        res = str.split("");
+        System.out.println(Arrays.asList(res));
+        res = str.split(" ");
+        System.out.println(Arrays.asList(res));
+        res = str.split("e");
+        System.out.println(Arrays.asList(res));
+        res = str.split("e|a");
+        System.out.println(Arrays.asList(res));
+        res = str.split("n|\\？");
+        System.out.println(Arrays.asList(res));
+    }
+}
---
+++ Base/src/main/java/com/benny/learning/tryCatchDemo/ComDemo.java
@@ -0,0 +1,4 @@
+package com.benny.learning.tryCatchDemo;
+
+public class ComDemo {
+}
