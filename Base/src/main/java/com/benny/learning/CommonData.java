253e844fe72b7e6ba0c31ad51af50cd9d45e8b21
BennyJane <3355817143@qq.com>
2021-03-26 22:51:21 +0800

    更新

---
+++ Base/src/main/java/com/benny/learning/CommonData.java
@@ -0,0 +1,18 @@
+package com.benny.learning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommonData {
+    public static List<String> servers = new ArrayList() {
+        {
+            add("Felordcn");
+            add("Tomcat");
+            add("Jetty");
+            add("Undertow");
+            add("Resin");
+        }
+    };
+
+
+}
