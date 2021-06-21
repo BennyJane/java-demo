628ed08d276de48ab5f1671ee12795a06a3aecb6
BennyJane <3355817143@qq.com>
2021-06-21 20:17:13 +0800

    更新

---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/array/middle/Q73.java
@@ -0,0 +1,31 @@
+package com.benny.learning.algorithm.leetcode.array.middle;
+
+
+import com.benny.learning.algorithm.utils.ListNode;
+
+/**
+ * 141 环形链表
+ */
+public class Q73 {
+    // 龟兔赛跑： 快慢指针
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return true;
+    }
+}
+
+
---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/array/middle/Q87.java
@@ -0,0 +1,6 @@
+package com.benny.learning.algorithm.leetcode.array.middle;
+
+public class Q87 {
+}
+
+
