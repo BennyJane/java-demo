beac16d860df0b65ad15935a3319c0d94b738380
BennyJane <3355817143@qq.com>
2021-06-24 21:50:53 +0800

    更新

---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/array/simple/Q23.java
@@ -0,0 +1,16 @@
+package com.benny.learning.algorithm.leetcode.array.simple;
+
+import com.benny.learning.utils.TreeNode;
+
+/**
+ * 653. 两数之和 IV - 输入 BST
+ * https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+ */
+public class Q23 {
+    public boolean findTarget(TreeNode root, int k) {
+        
+        return false;
+    }
+}
+
+
