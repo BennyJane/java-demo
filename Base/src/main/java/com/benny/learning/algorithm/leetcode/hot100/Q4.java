1687c059d1b2d1843f9e427398ba894346ba5e04
BennyJane <3355817143@qq.com>
2021-06-30 21:28:40 +0800

    更新

--- Base/src/main/java/com/benny/learning/algorithm/leetcode/hot100/Q4.java
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/hot100/Q4.java
@@ -1,8 +1,6 @@
 package com.benny.learning.algorithm.leetcode.hot100;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * 49. 字母异位词分组
@@ -51,6 +49,49 @@ public class Q4 {
         }
         return true;
     }
+
+    // 参考答案
+    // 字符串 转 数组，排序后，重新生成字符串
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);   // 重新转为字符串
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(s);
+            map.put(key, list);
+        }
+
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    public List<List<String>> groupAnagrams3(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str : strs) {
+            // 主要目的： 重新生成一个有序字符串
+            int[] counts = new int[26];
+            int length = str.length();
+            for (int i = 0; i < length; i++) {
+                counts[str.charAt(i) - 'a']++;
+            }
+            // 将每个出现次数大于 0 的字母和出现次数按顺序拼接成字符串，作为哈希表的键
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < 26; i++) {
+                if (counts[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(counts[i]);
+                }
+            }
+            String key = sb.toString();
+
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
 
     public static void main(String[] args) {
         Q4 q = new Q4();
--- Base/src/main/java/com/benny/learning/algorithm/leetcode/hot100/Q7.java
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/hot100/Q7.java
@@ -1,6 +1,17 @@
 package com.benny.learning.algorithm.leetcode.hot100;
 
+/**
+ * 221. 最大正方形
+ * https://leetcode-cn.com/problems/maximal-square/
+ */
 public class Q7 {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int maxWidth = Math.max(m, n);
+
+        return 0;
+    }
 }
 
 
