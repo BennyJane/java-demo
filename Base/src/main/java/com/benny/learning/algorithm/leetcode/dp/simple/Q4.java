ca8f6beb02257914ee5056b665e9424654da2a6b
BennyJane <3355817143@qq.com>
2021-07-03 22:33:27 +0800

    更新

---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/dp/simple/Q4.java
@@ -0,0 +1,16 @@
+package com.benny.learning.algorithm.leetcode.dp.simple;
+
+import java.util.List;
+
+/**
+ * 241. 为运算表达式设计优先级
+ * https://leetcode-cn.com/problems/different-ways-to-add-parentheses/
+ */
+public class Q4 {
+    public List<Integer> diffWaysToCompute(String expression) {
+
+        return null;
+    }
+}
+
+
---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/dp/simple/Q6.java
@@ -0,0 +1,6 @@
+package com.benny.learning.algorithm.leetcode.dp.simple;
+
+public class Q6 {
+}
+
+
