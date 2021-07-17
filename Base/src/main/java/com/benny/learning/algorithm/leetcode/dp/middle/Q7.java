a4de49f0c565bb908983c5e2a1a4d2dea21b1257
BennyJane <3355817143@qq.com>
2021-07-17 22:53:35 +0800

    更新

--- Base/src/main/java/com/benny/learning/algorithm/leetcode/dp/middle/Q7.java
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/dp/middle/Q7.java
@@ -1,6 +1,14 @@
 package com.benny.learning.algorithm.leetcode.dp.middle;
 
+/**
+ * 1043. 分隔数组以得到最大和
+ * https://leetcode-cn.com/problems/partition-array-for-maximum-sum/
+ */
 public class Q7 {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+
+        return 0;
+    }
 }
 
 
--- Base/src/main/java/com/benny/learning/algorithm/leetcode/dp/middle/Q8.java
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/dp/middle/Q8.java
@@ -1,6 +1,14 @@
 package com.benny.learning.algorithm.leetcode.dp.middle;
 
+/**
+ * 576. 出界的路径数
+ * https://leetcode-cn.com/problems/out-of-boundary-paths/
+ */
 public class Q8 {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+
+        return 0;
+    }
 }
 
 
