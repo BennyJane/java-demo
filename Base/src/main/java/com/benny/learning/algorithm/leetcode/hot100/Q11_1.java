958bed5b12122e810abc8634db1f353c5e120142
BennyJane <3355817143@qq.com>
2021-08-01 22:31:31 +0800

    更新

---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/hot100/Q11_1.java
@@ -0,0 +1,44 @@
+package com.benny.learning.algorithm.leetcode.hot100;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 11. 盛最多水的容器
+ * https://leetcode-cn.com/problems/container-with-most-water/
+ */
+public class Q11_1 {
+    // 贪心思想： 从暴力求解推导
+    public int maxArea(int[] height) {
+        int len = height.length;
+        int left = 0;
+        int right = len - 1;
+        int ans = 0;
+        while (left < right) {
+            int area = 0;
+            if (height[left] <= height[right]) {
+                area = height[left] * (right - left);
+                left++;
+            } else {
+                area = height[right] * (right - left);
+                right--;
+            }
+            ans = Math.max(ans, area);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Q11_1 q = new Q11_1();
+        int[] nums = new int[]{
+                4, 3, 2, 1, 4
+//                1, 1
+//                1, 8, 6, 2, 5, 4, 8, 3, 7
+
+        };
+        q.maxArea(nums);
+    }
+}
+
+
---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/hot100/Q12.java
@@ -0,0 +1,16 @@
+package com.benny.learning.algorithm.leetcode.hot100;
+
+/**
+ * 72. 编辑距离
+ * https://leetcode-cn.com/problems/edit-distance/
+ */
+public class Q12 {
+    public int minDistance(String word1, String word2) {
+
+
+        return 0;
+    }
+
+}
+
+
