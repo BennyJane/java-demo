267efa82074371334c625cbf0d8f74a83ef5bfdf
BennyJane <3355817143@qq.com>
2021-08-31 23:24:49 +0800

    更新

---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/tanxin/Q1.java
@@ -0,0 +1,14 @@
+package com.benny.learning.algorithm.leetcode.tanxin;
+
+/**
+ * https://leetcode-cn.com/problems/smallest-range-ii/
+ * 910. 最小差值 II
+ */
+public class Q1 {
+    public int smallestRangeII(int[] nums, int k) {
+
+        return 10;
+    }
+}
+
+
---
+++ Base/src/main/java/com/benny/learning/algorithm/leetcode/tanxin/Q9.java
@@ -0,0 +1,6 @@
+package com.benny.learning.algorithm.leetcode.tanxin;
+
+public class Q9 {
+}
+
+
