40e3a2522e0450287abd7956a51f171c81769017
BennyJane <3355817143@qq.com>
2022-04-21 15:37:50 +0800

    新增旋转数组

---
+++ algorithm-demo/src/main/java/org/example/com/high/interview/base/countEval.java
@@ -0,0 +1,284 @@
+package org.example.com.high.interview.base;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class countEval {
+}
+
+class Solution {
+    // TODO 1^((0|0)|1)和1^(0|0|1)，两者答案显然一样，但是一个有括号一个没有括号,
+    // 只算作一个答案
+
+    /**
+     * after 字符计算结果数量，重复累加
+     * [before]: 1^0|0|1|0
+     * [after]: 1^0|1|0
+     * <p>
+     * [before]: 1^0|0|1|0
+     * [after]: 1^0|1|0
+     */
+
+    public int countEval(String s, int result) {
+        if (s.length() == 1) {
+            int res = s.charAt(0) - '0';
+            System.out.println("res:   " + res);
+            System.out.println("======================================");
+            if (res == result) {
+                return 1;
+            }
+            return 0;
+        }
+        int ans = 0;
+        for (int i = 1; i < s.length(); i++) {
+            char sign = s.charAt(i);
+            if (Character.isDigit(sign)) continue;
+            int before = s.charAt(i - 1) - '0';
+            int after = s.charAt(i + 1) - '0';
+            int res = 0;
+            if (sign == '&') {
+                res = before & after;
+            } else if (sign == '|') {
+                res = before | after;
+            } else if (sign == '^') {
+                res = before ^ after;
+            } else {
+                continue;
+            }
+
+            String nxtS = s.substring(0, i - 1) + res + s.substring(i + 2);
+            if (s == "1^0|0|1|0") {
+                System.out.println("[before]: " + s);
+                System.out.println("[after]: " + nxtS);
+            }
+            int tmp = countEval(nxtS, result);
+
+            ans += tmp;
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+//        int res = s.countEval("1^0|0|1", 0);
+//        int res = s.countEval("0&0&0&1^1|0", 1);
+//        int res = s.countEval("0&0&0&1^1|0", 0);
+        int res = s.countEval("1^0|0|1|0", 0);
+        System.out.println(res);
+    }
+}
+
+class Solution2 {
+
+    // 记忆化搜索：需要记录字符串 + 结果值
+    private static Map<String, Integer> cache = new HashMap<>();
+
+    public int countEval(String s, int result) {
+        if (s.length() == 1) {
+            int res = s.charAt(0) - '0';
+            if (res == result) {
+                return 1;
+            }
+            return 0;
+        }
+        String key = s + "_" + result;
+        if (cache.containsKey(key)) return cache.get(key);
+        int ans = 0;
+        for (int i = 1; i < s.length(); i += 2) {
+            char sign = s.charAt(i);
+            if (result == 0) {
+                if (sign == '&') {
+                    ans += countEval(s.substring(0, i), 0) * countEval(s.substring(i + 1), 0);
+                    ans += countEval(s.substring(0, i), 1) * countEval(s.substring(i + 1), 0);
+                    ans += countEval(s.substring(0, i), 0) * countEval(s.substring(i + 1), 1);
+                }
+                if (sign == '|') {
+                    ans += countEval(s.substring(0, i), 0) * countEval(s.substring(i + 1), 0);
+                }
+                if (sign == '^') {
+                    ans += countEval(s.substring(0, i), 1) * countEval(s.substring(i + 1), 1);
+                    ans += countEval(s.substring(0, i), 0) * countEval(s.substring(i + 1), 0);
+                }
+            } else {
+                if (sign == '&') {
+                    ans += countEval(s.substring(0, i), 1) * countEval(s.substring(i + 1), 1);
+                }
+                if (sign == '|') {
+                    ans += countEval(s.substring(0, i), 0) * countEval(s.substring(i + 1), 1);
+                    ans += countEval(s.substring(0, i), 1) * countEval(s.substring(i + 1), 1);
+                    ans += countEval(s.substring(0, i), 1) * countEval(s.substring(i + 1), 0);
+                }
+                if (sign == '^') {
+                    ans += countEval(s.substring(0, i), 1) * countEval(s.substring(i + 1), 0);
+                    ans += countEval(s.substring(0, i), 0) * countEval(s.substring(i + 1), 1);
+                }
+            }
+        }
+        cache.put(key, ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution2 s = new Solution2();
+//        int res = s.countEval("1^0|0|1", 0);
+//        int res = s.countEval("0&0&0&1^1|0", 1);
+//        int res = s.countEval("0&0&0&1^1|0", 0);
+        int res = s.countEval("1^0|0|1|0", 0);
+        System.out.println(res);
+    }
+}
+
+
+class Solution3 {
+    Integer[][][] memo;
+
+    public int countEval(String s, int result) {
+        int n = s.length();
+        memo = new Integer[n][n][2];
+        return dfs(0, n - 1, s, result);
+    }
+
+    int dfs(int l, int r, String s, int result) { //区间[l, r]求result的括号方案数
+        if (l > r) return 0;
+        if (l == r) {
+            return (s.charAt(l) - '0') == result ? 1 : 0;
+        }
+        // FIXME 默认值是null？？
+        if (memo[l][r][result] != null) return memo[l][r][result];
+        int ans = 0;
+        for (int i = l; i <= r; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) continue;
+            if (result == 0) {
+                if (c == '&')
+                    ans += dfs(l, i - 1, s, 0) * dfs(i + 1, r, s, 0) + dfs(l, i - 1, s, 0) * dfs(i + 1, r, s, 1) + dfs(l, i - 1, s, 1) * dfs(i + 1, r, s, 0); //00、01、10
+                if (c == '|') ans += dfs(l, i - 1, s, 0) * dfs(i + 1, r, s, 0); //00
+                if (c == '^')
+                    ans += dfs(l, i - 1, s, 0) * dfs(i + 1, r, s, 0) + dfs(l, i - 1, s, 1) * dfs(i + 1, r, s, 1); //00、11
+            } else {
+                if (c == '&') ans += dfs(l, i - 1, s, 1) * dfs(i + 1, r, s, 1); //11
+                if (c == '|')
+                    ans += dfs(l, i - 1, s, 0) * dfs(i + 1, r, s, 1) + dfs(l, i - 1, s, 1) * dfs(i + 1, r, s, 0) + dfs(l, i - 1, s, 1) * dfs(i + 1, r, s, 1); //01、10、11
+                if (c == '^')
+                    ans += dfs(l, i - 1, s, 0) * dfs(i + 1, r, s, 1) + dfs(l, i - 1, s, 1) * dfs(i + 1, r, s, 0); //10、01
+            }
+        }
+        return memo[l][r][result] = ans;
+    }
+}
+
+
+class Solution4 {
+    public int countEval(String s, int result) {
+        //特例
+        if (s.length() == 0) {
+            return 0;
+        }
+        if (s.length() == 1) {
+            return (s.charAt(0) - '0') == result ? 1 : 0;
+        }
+        char[] ch = s.toCharArray();
+        //定义状态: 给定区间上执行结果只有两种
+        int[][][] dp = new int[ch.length][ch.length][2];
+        //base case：长度为1的计算结果
+        // 只处理left <= right的范围，只用到一半区间
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] == '0' || ch[i] == '1') {
+                dp[i][i][ch[i] - '0'] = 1;
+            }
+        }
+        //套区间dp模板
+        //枚举区间长度len，跳步为2，一个数字一个符号
+        for (int len = 2; len <= ch.length; len += 2) {
+            //枚举区间起点，数字位，跳步为2
+            for (int i = 0; i <= ch.length - len; i += 2) {
+                //区间终点，数字位
+                int j = i + len;
+                //枚举分割点，三种 '&','|', '^'，跳步为2
+                for (int k = i + 1; k <= j - 1; k += 2) {
+                    if (ch[k] == '&') {
+                        //结果为0 有三种情况： 0 0, 0 1, 1 0
+                        //结果为1 有一种情况： 1 1
+                        dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0] + dp[i][k - 1][0] * dp[k + 1][j][1] + dp[i][k - 1][1] * dp[k + 1][j][0];
+                        dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][1];
+                    }
+                    if (ch[k] == '|') {
+                        //结果为0 有一种情况： 0 0
+                        //结果为1 有三种情况： 0 1, 1 0, 1 1
+                        dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0];
+                        dp[i][j][1] += dp[i][k - 1][0] * dp[k + 1][j][1] + dp[i][k - 1][1] * dp[k + 1][j][0] + dp[i][k - 1][1] * dp[k + 1][j][1];
+                    }
+                    if (ch[k] == '^') {
+                        //结果为0 有两种情况： 0 0, 1 1
+                        //结果为1 有两种情况： 0 1, 1 0
+                        dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0] + dp[i][k - 1][1] * dp[k + 1][j][1];
+                        dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][0] + dp[i][k - 1][0] * dp[k + 1][j][1];
+                    }
+                }
+            }
+        }
+        return dp[0][ch.length - 1][result];
+    }
+
+    // FIXME 错误！！！ 状态转移方程错误，前一个状态还没有计算出来
+    public int countEval2(String s, int result) {
+        if (s.length() == 1) {
+            return (s.charAt(0) - '0') == result ? 1 : 0;
+        }
+        char[] ch = s.toCharArray();
+        int n = ch.length;
+        //定义状态: 给定区间上执行结果只有两种
+        int[][][] dp = new int[n][n][2];
+        //base case：长度为1的计算结果
+        // 只处理left <= right的范围，只用到一半区间
+        for (int i = 0; i < n; i++) {
+            if (ch[i] == '0' || ch[i] == '1') {
+                dp[i][i][ch[i] - '0'] = 1;
+            }
+        }
+
+        // TODO 错误思想：直接单个起点，遍历所有情况
+        for (int i = 0; i < n; i += 2) {
+            for (int j = i + 2; j < n; j += 2) {
+                // k 枚举符号位置
+                for (int k = i + 1; k <= j - 1; k += 2) {
+                    char kChar = ch[k];
+                    switch (kChar) {
+                        case '&':
+                            // & 计算得到0的情况： 00，01，10
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0];
+                            dp[i][j][0] += dp[i][k - 1][1] * dp[k + 1][j][0];
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][1];
+                            // & 计算得到1的情况： 11
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][1];
+                            break;
+                        case '|':
+                            // | 计算得到0的情况： 00
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][0];
+                            // | 计算得到1的情况： 11，01，10
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][1];
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][0];
+                            dp[i][j][1] += dp[i][k - 1][0] * dp[k + 1][j][1];
+                            break;
+                        case '^':
+                            // 计算得到0的情况： 01， 10
+                            dp[i][j][0] += dp[i][k - 1][0] * dp[k + 1][j][1];
+                            dp[i][j][0] += dp[i][k - 1][1] * dp[k + 1][j][0];
+                            // 计算得到1的情况： 00， 11
+                            dp[i][j][1] += dp[i][k - 1][1] * dp[k + 1][j][1];
+                            dp[i][j][1] += dp[i][k - 1][0] * dp[k + 1][j][0];
+                            break;
+                    }
+                }
+            }
+        }
+        System.out.println(dp);
+        return dp[0][ch.length - 1][result];
+    }
+
+    public static void main(String[] args) {
+        Solution4 s = new Solution4();
+        s.countEval2("1^0|0|1|0", 0);
+    }
+}
--- algorithm-demo/src/main/java/org/example/com/leetcode/year2022/month04/Q20.java
+++ algorithm-demo/src/main/java/org/example/com/leetcode/year2022/month04/Q20.java
@@ -4,9 +4,13 @@ package org.example.com.leetcode.year2022.month04;
  * 388. 文件的最长绝对路径
  */
 public class Q20 {
-    public int lengthLongestPath(String input) {
+    public static void main(String[] args) {
 
-
-        return 0;
+        char n = '1';
+        int num = 1;
+        char ans = (char) (num + 48);
+        System.out.println(num + '0');
+        System.out.println(num - '0');
+        System.out.println(ans);
     }
 }
