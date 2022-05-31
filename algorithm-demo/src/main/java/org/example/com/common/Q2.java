4472b40d11c18eb0ec7fc96094f3c57764db2aab
BennyJane <3355817143@qq.com>
2022-05-31 10:09:33 +0800

    更新文档

---
+++ algorithm-demo/src/main/java/org/example/com/common/Q2.java
@@ -0,0 +1,20 @@
+package org.example.com.common;
+
+public class Q2 {
+
+
+    public static void main(String[] args) {
+        int[] a = {474833169, 264817709, 998097157, 817129560};
+        int[] b = {197493099, 404280278, 893351816, 505795335};
+
+
+        System.out.println("abc".contains("b"));
+        System.out.println("abc".contains("b"));
+
+        System.out.println("172.16.254.1".contains("."));
+        System.out.println("172.16.254.1".split("."));
+
+
+
+    }
+}
---
+++ java-base-demo/src/main/java/org/example/com/intDemo/Demo.java
@@ -0,0 +1,8 @@
+package org.example.com.intDemo;
+
+public class Demo {
+    public static void main(String[] args) {
+        int res = Integer.parseInt("101", 2);
+        System.out.println(res);
+    }
+}
